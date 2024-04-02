978dc3041a5b0beda0a7411ee20073a82b3e2c6a
AugusttoDaniel <danielsje7133@gmail.com>
2024-04-02 18:04:58 -0300

    .

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cloud = document.querySelector('.cloud');
 const gameOver = document.querySelector('.game-over');
 const restartButton = document.querySelector('.restart');
 const scoreDisplay = document.querySelector('.score');
+const scoreGameHover = document.querySelector('.game-over-score');
 const highScoresDisplay = document.querySelector('.high-scores');
 
 let score = 0;
@@ -27,20 +28,36 @@ function startScore() {
 function stopScoreAndUpdateHighScores() {
     clearInterval(scoreInterval);
     // Verifica se o score atual e o nome estão sendo corretamente adicionados ao array de highScores
+    console.log("Before pushing new score", highScores);
     highScores.push({ score: score, name: playerName });
-    // Certifique-se de que a ordenação está considerando o objeto com score e nome
+    console.log("After pushing new score", highScores);    // Certifique-se de que a ordenação está considerando o objeto com score e nome
     highScores.sort((a, b) => b.score - a.score);
     // Mantém apenas os 5 melhores scores
     highScores = highScores.slice(0, 5);
     // Atualiza os highScores no localStorage
+    scoreGameHover.textContent = `Score: ${score}`;
     localStorage.setItem('highScores', JSON.stringify(highScores));
     // Atualiza a exibição dos highScores
     updateHighScoresDisplay();
 }
 
 function updateHighScoresDisplay() {
-    highScoresDisplay.innerHTML = 'Best Scores:<br>' + highScores.map((item, index) => `${index + 1}. ${item.name}: ${item.score}`).join('<br>');
+    // Check if the highScores array is defined and has at least one score
+    if (highScores && highScores.length > 0) {
+        highScoresDisplay.innerHTML = 'Best Scores:<br>' + highScores.map((item, index) => {
+            // Check if both name and score are defined for the current item
+            if (item && item.hasOwnProperty('name') && item.hasOwnProperty('score')) {
+                return `${index + 1}. ${item.name}: ${item.score}`;
+            } else {
+                return `${index + 1}. Player: Score not available`;
+            }
+        }).join('<br>');
+    } else {
+        // If no scores are available, display a default message
+        highScoresDisplay.innerHTML = 'Best Scores:<br>No high scores available.';
+    }
 }
+
 
 function jump() {
     mario.classList.add('jump');
